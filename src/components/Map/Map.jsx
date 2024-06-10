c67569834ef42f3b4a850937e2e95bc05e4426d1
Katiasun <katjatolok3@gmail.com>
2024-06-10 14:15:09 +0200

    mapStyles - object with styles for the map (height and width). defaultCenter - the initial position of the center of the map (latitude and longitude).LoadScript - a component for loading Google Maps API scripts. GoogleMap - a component for displaying a map. mapContainerStyle - map container styles. zoom - map magnification level.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,21 @@ import React, { useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import styles from "./styles.module.css";
 
+const mapStyles = {
+  height: "100vh",
+  width: "100%",
+};
+
+const defaultCenter = {
+  lat: 41.3851,
+  lng: 2.1734,
+};
+
 export default function Map() {
   const [selectedPosition, setSelectedPosition] = useState(null);
-  return <></>;
+  return (
+    <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
+      <GoogleMap mapContainerStyle={mapStyles} zoom={13} />
+    </LoadScript>
+  );
 }
