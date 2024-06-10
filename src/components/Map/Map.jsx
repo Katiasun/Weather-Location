6dbb1754082042d3cd1cb83ca3ac3f7133d4d101
Katiasun <katjatolok3@gmail.com>
2024-06-10 13:15:37 +0200

    selectedPosition - state for saving the selected position on the map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,5 +3,6 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import styles from "./styles.module.css";
 
 export default function Map() {
+  const [selectedPosition, setSelectedPosition] = useState(null);
   return <></>;
 }
