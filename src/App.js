447813532893f73f5afb96f9829bf4c5b0c98bac
Katiasun <katjatolok3@gmail.com>
2024-06-10 13:00:59 +0200

    delete EventForm and EventList components. Remove the redundant code in the file App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EventList from "./components/eventList/EventList.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/">
-            {/* element={<>
-              <EventForm />
-            <Weather/>
-            </>} */}
-          </Route>
-
-          <Route path="/eventList" element={<EventList />} />
-        </Routes>
-      </Router>
+      <Router></Router>
     </div>
   );
 }
