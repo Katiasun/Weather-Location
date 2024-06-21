f315946854bd4e8113987d8d6a9c24044cb13e64
Katiasun <katjatolok3@gmail.com>
2024-06-21 22:27:13 +0200

    add class in file SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,7 @@
 import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import styles from "./styles.module.css";
 
-export default function SearchBar({ onSelectLocation }) {}
+export default function SearchBar({ onSelectLocation }) {
+  return <div className={styles.searchBar}></div>;
+}
