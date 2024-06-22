af33fbb4ea2fb6012688bc1b3cd054dc19c0b7ab
Katiasun <katjatolok3@gmail.com>
2024-06-22 23:02:10 +0200

    handleSelect - This piece of code handles user selections in the SearchBar component. This function is declared as asynchronous (async) because asynchronous operations (working with the API) are used internally.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,5 +3,28 @@ import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import styles from "./styles.module.css";
 
 export default function SearchBar({ onSelectLocation }) {
-  return <div className={styles.searchBar}></div>;
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (address) => {
+    setValue(address.label, false);
+    clearSuggestions();
+
+    return (
+      <div className={styles.searchBar}>
+        <GooglePlacesAutocomplete
+          selectProps={{
+            value,
+            onChange: handleSelect,
+            onInputChange: () => setValue(inputValue),
+          }}
+        ></GooglePlacesAutocomplete>
+      </div>
+    );
+  };
 }
