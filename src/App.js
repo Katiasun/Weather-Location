0f567d157047f0759765e9204bef2083041bb81c
Katiasun <katjatolok3@gmail.com>
2024-06-26 14:36:23 +0200

    Integration of SearchBar in App. We pass handleSelectLocation as a prop onSelectLocation to SearchBar. It is used both for processing the selection of a place on the map and for selection through the search bar.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Map from "./components/Map/Map.jsx";
+import SearchBar from "./components/SearchBar/SearchBar.jsx";
 
 function App() {
   const [center, setCenter] = useState({ lat: 41.3851, lng: 2.1734 });
@@ -14,7 +15,15 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Map center={center} onSelect={handleSelectLocation} />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar onSelectLocation={handleSelectLocation} />
+                <Map center={center} onSelect={handleSelectLocation} />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
