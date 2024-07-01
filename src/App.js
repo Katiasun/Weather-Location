a2be9f48c9b44497bf468cee6797206c2a90db01
Katiasun <katjatolok3@gmail.com>
2024-07-01 22:02:17 +0200

    added a line that will render the WeatherInfo component with the passed data

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
                 <>
                   <SearchBar onSelectLocation={handleSelectLocation} />
                   <Map center={center} onSelect={handleSelectLocation} />
+                  {weather && <WeatherInfo weather={weather} />}
                 </>
               }
             />
