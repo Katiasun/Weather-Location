5709bb1e769431ed6ec0b236a50d214841485af2
Katiasun <katjatolok3@gmail.com>
2024-07-12 00:11:20 +0200

    add name of classes in component WeatherInfo

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import styles from "./WeatherInfo.module.css";
 
 export default function WeatherInfo({ weather }) {
   return (
-    <div>
-      <h3>Weather Information</h3>
-      <p>Location: {weather.name}</p>
-      <p>Temperature: {weather.main.temp}°C</p>
-      <p>Condition: {weather.weather[0].description}</p>
+    <div className={styles.WeatherInfo}>
+      <h3 className={styles.weatherTitle}>Weather Information</h3>
+      <p className={styles.weatherDetail}>Location: {weather.name}</p>
+      <p className={styles.weatherDetail}>Temperature: {weather.main.temp}°C</p>
+      <p className={styles.weatherDetail}>
+        <span className={styles.weatherLabel}>Condition:</span> {weather.weather[0].description}
+      </p>
     </div>
   );
 }
