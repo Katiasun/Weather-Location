1dc5573d49168fa8cee39761d6c1b87eab172674
Katiasun <katjatolok3@gmail.com>
2024-07-12 22:54:55 +0200

    write styles for component HistoryPanel

--- src/components/HistoryPanel/HistoryPanel.jsx
+++ src/components/HistoryPanel/HistoryPanel.jsx
@@ -4,7 +4,7 @@ import styles from "./HistoryPanel.module.css";
 export default function HistoryPanel({ history }) {
   return (
     <div className={styles.historyPanel}>
-      <h3>History</h3>
+      <h3 className={styles.historyTitle}>History</h3>
       <ul className={styles.historyList}>
         {history.map((point, index) => (
           <li key={index} className={styles.historyItem}>
