f3ae9fe8692136b75f8d73a3f3ff0def222f4286
Katiasun <katjatolok3@gmail.com>
2024-07-17 22:24:29 +0200

    Write code for component WeatherTooltip

--- src/components/WeatherTooltip/WeatherTooltip/WeatherToolyip.jsx
+++ src/components/WeatherTooltip/WeatherTooltip/WeatherToolyip.jsx
@@ -1,3 +1,25 @@
 import React from "react";
 import { InfoWindow } from "@react-google-maps/api";
 import styles from "./WeatherToolyipple.css";
+
+export default function WeatherToolyipple({ position, weather, onClose }) {
+  return (
+    <InfoWindow position={position} onCloseClick={onClose}>
+      <div className={styles.weatherTooltip}>
+        <h3 className={styles.weatherTitle}>Weather Information</h3>
+        <p className={styles.weatherDetail}>
+          <span className={styles.weatherLabel}>Location: </span>
+          {weather.name}
+        </p>
+        <p className={styles.weatherDetail}>
+          <span className={styles.weatherLabel}>Temperature: </span>
+          {weather.main.temp}°C
+        </p>
+        <p className={styles.weatherDetail}>
+          <span className={styles.weatherLabel}>Condition: </span>
+          {weather.weather[0].description}
+        </p>
+      </div>
+    </InfoWindow>
+  );
+}
