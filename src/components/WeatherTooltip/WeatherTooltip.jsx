1ed750e35e7c5c6785efd28e085dd4dda79e6b52
Katiasun <katjatolok3@gmail.com>
2024-07-22 23:23:22 +0200

    Customize close button in WeatherTooltip
    - Added useEffect to apply custom styles to the close button in InfoWindow component from @react-google-maps/api.
    - Adjusted close button styles: increased font size, changed color, set width and height, removed border, and set cursor to pointer.
    - Added pixelOffset option to InfoWindow for better positioning.

--- src/components/WeatherTooltip/WeatherTooltip.jsx
+++ src/components/WeatherTooltip/WeatherTooltip.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { InfoWindow } from "@react-google-maps/api";
 import styles from "./WeatherTooltip.module.css";
 
 export default function WeatherTooltip({ position, weather, onClose }) {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const closeButton = document.querySelector(".gm-ui-hover-effect");
+
+      if (closeButton) {
+        closeButton.style.fontSize = "16px";
+        closeButton.style.color = "#5f6368";
+        closeButton.style.width = "40px";
+        closeButton.style.height = "24px";
+        closeButton.style.border = "none";
+        closeButton.style.cursor = "pointer";
+
+        clearInterval(intervalId);
+      }
+    }, 100);
+  }, []);
+
   return (
-    <InfoWindow position={position} onCloseClick={onClose}>
+    <InfoWindow
+      position={position}
+      onCloseClick={onClose}
+      options={{ pixelOffset: new window.google.maps.Size(0, -30) }}
+      className={styles.customInfowindow}
+    >
       <div className={styles.weatherTooltip}>
         <h3 className={styles.weatherTitle}>Weather Information</h3>
         <p className={styles.weatherDetail}>
