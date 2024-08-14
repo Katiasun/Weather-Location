b258b22a2a97c89c591c86b847753eb893afbe76
Katiasun <katjatolok3@gmail.com>
2024-08-14 14:39:02 +0200

    efactor WeatherForecast component
    - Created new component WeatherForecastItem for displaying individual weather forecast items.
    - Moved date formatting logic and other item-specific details to WeatherForecastItem.

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,27 +1,6 @@
 import React from "react";
 import styles from "./WeatherForecast.module.css";
-import { WiDaySunny, WiDayCloudy, WiRain, WiSnow, WiThunderstorm, WiCloudy } from "react-icons/wi";
-
-const iconMapping = {
-  "01d": <WiDaySunny />,
-  "02d": <WiDayCloudy />,
-  "03d": <WiCloudy />,
-  "04d": <WiCloudy />,
-  "09d": <WiRain />,
-  "10d": <WiRain />,
-  "11d": <WiThunderstorm />,
-  "13d": <WiSnow />,
-  "50d": <WiCloudy />,
-  "01n": <WiDaySunny />,
-  "02n": <WiDayCloudy />,
-  "03n": <WiCloudy />,
-  "04n": <WiCloudy />,
-  "09n": <WiRain />,
-  "10n": <WiRain />,
-  "11n": <WiThunderstorm />,
-  "13n": <WiSnow />,
-  "50n": <WiCloudy />,
-};
+import WeatherForecastItem from "./WeatherForecastItem/WeatherForecastItem";
 
 export default function WeatherForecast({ forecast, isVisible }) {
   return (
@@ -30,32 +9,9 @@ export default function WeatherForecast({ forecast, isVisible }) {
         <h3>Weather Forecast</h3>
       </div>
       <div className={styles.weatherForecastContent}>
-        {forecast.map((data, index) => {
-          const weatherIcon = iconMapping[data.weather[0].icon];
-
-          return (
-            <div key={index} className={styles.weatherForecastItem}>
-              <div className={styles.weatherForecastDay}>
-                {new Date(data.dt * 1000).toLocaleDateString("en-GB", { weekday: "short" })}
-              </div>
-              <div className={styles.weatherForecastDate}>
-                {new Date(data.dt * 1000).toLocaleDateString("en-GB", {
-                  day: "2-digit",
-                  month: "short",
-                })}
-              </div>
-              <div className={styles.weatherForecastIcon}>{weatherIcon}</div>
-              <div className={styles.weatherForecastTemper}>
-                <span className={styles.tempDay}>{Math.round(data.main.temp_max)}°C</span>
-                <span className={styles.tempNight}>{Math.round(data.main.temp_min)}°C</span>
-              </div>
-              <div className={styles.weatherForecastWind}>Wind: {data.wind.speed} m/s</div>
-              <div className={styles.weatherForecastRain}>
-                Precipitation: {data.rain ? data.rain["3h"] : 0} mm
-              </div>
-            </div>
-          );
-        })}
+        {forecast.map((data, index) => (
+          <WeatherForecastItem key={index} data={data} />
+        ))}
       </div>
     </div>
   );
